9e458ccdb5303354b41c0833f7cf4138022749db
BT-Matshazi <bekithembamatshazi@gmail.com>
2023-08-05 20:37:51 +0200

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,22 @@ import Footer from './components/Footer';
 import Intro from './components/Intro';
 import Portfolio from './components/Portfolio';
 import Timeline from './components/Timeline';
+import Hero from './components/Hero';
 
 function App() {
   return (
-	<>
-		<div className="bg-white dark:bg-stone-900 text-stone-900 dark:text-stone-300 min-h-screen font-inter">
-			<div className="max-w-5xl w-11/12 mx-auto">
-				<Intro />
-				<Portfolio />
-				<Timeline />
-				<Footer />
-			</div>
-		</div>
-	 </>
-  )
+    <>
+      <div className="bg-white dark:bg-stone-900 text-stone-900 dark:text-stone-300 min-h-screen font-inter">
+        <div className="max-w-[100%] mx-auto">
+          <Intro />
+          <Hero />
+          <Portfolio />
+          <Timeline />
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import portfolio from '../data/portfolio';
-import PortfolioItem from './PortfolioItem';
+import PortfolioItem from './PortfolioItem';;
 
 function Portfolio() {
    return (
-      <div className="flex flex-col md:flex-row items-center justify-center">
+     <>
+       <h1 className="text-2xl font-bold underline underline-offset-8 decoration-4 mb-5 text-stone-900 dark:text-white text-center">
+         Projects
+       </h1>
+       <div className="flex flex-col md:flex-row items-center justify-center">
          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {portfolio.map(project => (
-               <PortfolioItem 
-                  imgUrl={project.imgUrl}
-                  title={project.title}
-                  stack={project.stack}
-                  link={project.link}
-               />
-            ))}
+           {portfolio.map((project) => (
+             <PortfolioItem
+               imgUrl={project.imgUrl}
+               title={project.title}
+               stack={project.stack}
+               link={project.link}
+             />
+           ))}
          </div>
-      </div>
-   )
+       </div>
+     </>
+   );
 }
 
 export default Portfolio;
