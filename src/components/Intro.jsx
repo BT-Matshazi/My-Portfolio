8390f566e90a96ec64943e5bbdb50a0998675275
BT-Matshazi <bekithembamatshazi@gmail.com>
2023-08-05 20:47:21 +0200

    fixes

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,11 +6,6 @@ function Intro() {
       <nav class="bg-white border-gray-200 dark:bg-gray-900">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="" class="flex items-center">
-            <img
-              src="/assets/Bekithemba.jpg"
-              class="h-8 mr-3"
-              alt="Flowbite Logo"
-            />
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
              My Portfolio
             </span>
