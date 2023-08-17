f09f5e964f950432569de17d46cbac36707cca69
BT-Matshazi <bekithembamatshazi@gmail.com>
2023-08-17 03:05:33 +0200

    fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,7 @@ function Hero() {
               and intuitive design.
             </p>
             <a
-              href="/public/docs/CV - Bekithemba Matshazi.pdf"
+              href="/docs/CV - Bekithemba Matshazi.pdf"
               className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
             >
               My Resume
