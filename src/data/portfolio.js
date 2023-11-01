44a9d2765a1ae6cbcd218bda03c214c82998b46b
BT-Matshazi <bekithembamatshazi@gmail.com>
2023-11-02 01:28:43 +0200

    chat update

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -49,6 +49,6 @@ export default [
     stack: ["Next.js", "AWS S3", "SCSS", "Redux", "MapBox", "Bootstrap"],
     link: "https://roomsekasi.co.za/",
     description:
-      "Roomsekasi is a cutting-edge online platform designed to revolutionize the way users post and sell retail properties. Leveraging a powerful stack of technologies including Next.js, AWS S3, SCSS, Redux, MapBox, and Bootstrap, Roomsekasi offers a seamless and feature-rich experience for property listing, marketing, and transactions. Roomsekasi aims to become the go-to platform for retail property listings, catering to property owners, real estate agents, and potential buyers. Whether you're selling or searching for retail spaces, Roomsekasi promises a seamless, visually appealing, and efficient experience for all your retail property needs.",
+      "Roomsekasi is an online platform designed to allow users post and sell properties. Leveraging a powerful stack of technologies including Next.js, SCSS, Redux, MapBox, and Bootstrap, Roomsekasi is a seamless & feature-rich experience for property listing & marketing. Roomsekasi aims to become the go-to platform for retail property listings, catering to property owners, real estate agents, and potential buyers. Roomsekasi promises a seamless, visually appealing, and efficient experience for all your retail property needs.",
   },
 ];
