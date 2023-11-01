f89f772cf364685460aea1ae024976404364fc39
BT-Matshazi <bekithembamatshazi@gmail.com>
2023-11-01 10:13:40 +0200

    try

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,15 +1,15 @@
-import Roomsekasi1 from "/public/assets/roomsekasi project.jpg";
+import Roomsekasi1 from "/assets/roomsekasi project.jpg";
 
 
 
 //blackbox v2 images
-import BlackBoxV21 from "/public/images/portolio/BlackBox/v21.png";
-import BlackBoxV22 from "/public/images/portolio/BlackBox/v22.png";
-import BlackBoxV23 from "/public/images/portolio/BlackBox/v23.png";
-import BlackBoxV24 from "/public/images/portolio/BlackBox/v24.png";
-import BlackBoxV25 from "/public/images/portolio/BlackBox/v25.png";
-import BlackBoxV26 from "/public/images/portolio/BlackBox/v26.png";
-import BlackBoxV27 from "/public/images/portolio/BlackBox/v27.png";
+import BlackBoxV21 from "/images/portolio/BlackBox/v21.png";
+import BlackBoxV22 from "/images/portolio/BlackBox/v22.png";
+import BlackBoxV23 from "/images/portolio/BlackBox/v23.png";
+import BlackBoxV24 from "/images/portolio/BlackBox/v24.png";
+import BlackBoxV25 from "/images/portolio/BlackBox/v25.png";
+import BlackBoxV26 from "/images/portolio/BlackBox/v26.png";
+import BlackBoxV27 from "/images/portolio/BlackBox/v27.png";
 
 export default [
   {
